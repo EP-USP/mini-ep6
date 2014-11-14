f98735bb60bf56be3c0c16d5422b9d8c617807d4
Ian Carvalho <iancarv@gmail.com>
2014-11-14 11:31:04 -0200

    non patterned

---
+++ media.py
@@ -0,0 +1,33 @@
+class Media():
+    def __init__(self):
+        raise NotImplementedError('Media e uma classe abstrata \
+                                    e nao deve ser instanciada!')
+
+
+class DVD(Media):
+    def __init__(self, title):
+        self.title = title
+        self.name = 'DVD'
+
+    def play(self):
+        print ('Playing a nice ' + instance.title + '\'s ' +
+            ' DVD at my big TV')
+
+
+class CD(Media):
+    def __init__(self, title):
+        self.title = title
+        self.name = 'CD'
+
+    def play(self):
+        print ('Playing a nice ' + instance.title + '\'s ' +
+            ' CD at my big Radio')
+
+
+class MP3(Media):
+    def __init__(self, title):
+        self.title = title
+        self.name = 'MP3'
+
+    print ('Playing a nice ' + instance.title + '\'s ' +
+            ' MP3 at my big Radio')
